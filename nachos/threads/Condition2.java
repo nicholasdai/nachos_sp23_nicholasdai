1b720438d3d07aa205059e6645c5c32b25908101
nicholasdai <60338146+nicholasdai@users.noreply.github.com>
2023-05-03 16:34:40 -0700

    Update Condition2.java

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -2,17 +2,56 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+import java.util.LinkedList;
+
 /**
- * An implementation of condition variables that disables interrupt()s for
- * synchronization.
+ * An implementation of condition variables built upon semaphores.
  * 
  * <p>
- * You must implement this.
+ * A condition variable is a synchronization primitive that does not have a
+ * value (unlike a semaphore or a lock), but threads may still be queued.
  * 
- * @see nachos.threads.Condition
+ * <p>
+ * <ul>
+ * 
+ * <li><tt>sleep()</tt>: atomically release the lock and relinkquish the CPU
+ * until woken; then reacquire the lock.
+ * 
+ * <li><tt>wake()</tt>: wake up a single thread sleeping in this condition
+ * variable, if possible.
+ * 
+ * <li><tt>wakeAll()</tt>: wake up all threads sleeping inn this condition
+ * variable.
+ * 
+ * </ul>
+ * 
+ * <p>
+ * Every condition variable is associated with some lock. Multiple condition
+ * variables may be associated with the same lock. All three condition variable
+ * operations can only be used while holding the associated lock.
+ * 
+ * <p>
+ * In Nachos, condition variables are summed to obey <i>Mesa-style</i>
+ * semantics. When a <tt>wake()</tt> or <tt>wakeAll()</tt> wakes up another
+ * thread, the woken thread is simply put on the ready list, and it is the
+ * responsibility of the woken thread to reacquire the lock (this reacquire is
+ * taken core of in <tt>sleep()</tt>).
+ * 
+ * <p>
+ * By contrast, some implementations of condition variables obey
+ * <i>Hoare-style</i> semantics, where the thread that calls <tt>wake()</tt>
+ * gives up the lock and the CPU to the woken thread, which runs immediately and
+ * gives the lock and CPU back to the waker when the woken thread exits the
+ * critical section.
+ * 
+ * <p>
+ * The consequence of using Mesa-style semantics is that some other thread can
+ * acquire the lock and change data structures, before the woken thread gets a
+ * chance to run. The advance to Mesa-style semantics is that it is a lot easier
+ * to implement.
  */
 public class Condition2 {
-	/**
+	/**2
 	 * Allocate a new condition variable.
 	 * 
 	 * @param conditionLock the lock associated with this condition variable.
@@ -21,6 +60,8 @@ public class Condition2 {
 	 */
 	public Condition2(Lock conditionLock) {
 		this.conditionLock = conditionLock;
+		alarm = new Alarm();
+		waitQueue = new LinkedList<KThread>();
 	}
 
 	/**
@@ -28,13 +69,37 @@ public class Condition2 {
 	 * variable until another thread wakes it using <tt>wake()</tt>. The current
 	 * thread must hold the associated lock. The thread will automatically
 	 * reacquire the lock before <tt>sleep()</tt> returns.
+	 * 
+	 * <p>
+	 * This implementation uses semaphores to implement this, by allocating a
+	 * semaphore for each waiting thread. The waker will <tt>V()</tt> this
+	 * semaphore, so thre is no chance the sleeper will miss the wake-up, even
+	 * though the lock is released before caling <tt>P()</tt>.
 	 */
 	public void sleep() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-		conditionLock.release();
+		boolean status = Machine.interrupt().disable();
+		waitQueue.add(KThread.currentThread());
 
+		conditionLock.release();
+		KThread.sleep();
 		conditionLock.acquire();
+		Machine.interrupt().restore(status);
+
+	}
+
+	public void sleepFor(long timeout) {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+		boolean status = Machine.interrupt().disable();
+		waitQueue.add(KThread.currentThread());
+
+		conditionLock.release();
+		alarm.waitUntil(timeout);
+		conditionLock.acquire();
+		Machine.interrupt().restore(status);
+
 	}
 
 	/**
@@ -43,6 +108,15 @@ public class Condition2 {
 	 */
 	public void wake() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+		boolean status = Machine.interrupt().disable();
+
+		if (!waitQueue.isEmpty()) {
+			KThread temp = waitQueue.removeFirst();
+			if(!alarm.cancel(temp)) temp.ready();
+		}
+
+		Machine.interrupt().restore(status);
 	}
 
 	/**
@@ -51,19 +125,89 @@ public class Condition2 {
 	 */
 	public void wakeAll() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		boolean status = Machine.interrupt().disable();
+
+		while (!waitQueue.isEmpty())
+			wake();
+		
+		Machine.interrupt().restore(status);
 	}
 
-        /**
-	 * Atomically release the associated lock and go to sleep on
-	 * this condition variable until either (1) another thread
-	 * wakes it using <tt>wake()</tt>, or (2) the specified
-	 * <i>timeout</i> elapses.  The current thread must hold the
-	 * associated lock.  The thread will automatically reacquire
-	 * the lock before <tt>sleep()</tt> returns.
-	 */
-        public void sleepFor(long timeout) {
-		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+	private Lock conditionLock;
+
+	private LinkedList<KThread> waitQueue;
+
+	private Alarm alarm;
+
+	// Place Condition2 testing code in the Condition2 class.
+
+	// Example of the "interlock" pattern where two threads strictly
+	// alternate their execution with each other using a condition
+	// variable.  (Also see the slide showing this pattern at the end
+	// of Lecture 6.)
+
+	private static class InterlockTest {
+		private static Lock lock;
+		private static Condition2 cv;
+
+		private static class Interlocker implements Runnable {
+			public void run () {
+				lock.acquire();
+				for (int i = 0; i < 10; i++) {
+					System.out.println(KThread.currentThread().getName());
+					cv.wake();   // signal
+					cv.sleep();  // wait
+				}
+				lock.release();
+			}
+		}
+
+		public InterlockTest () {
+			lock = new Lock();
+			cv = new Condition2(lock);
+
+			KThread ping = new KThread(new Interlocker());
+			ping.setName("ping");
+			KThread pong = new KThread(new Interlocker());
+			pong.setName("pong");
+
+			ping.fork();
+			pong.fork();
+
+			// We need to wait for ping to finish, and the proper way
+			// to do so is to join on ping.  (Note that, when ping is
+			// done, pong is sleeping on the condition variable; if we
+			// were also to join on pong, we would block forever.)
+			// For this to work, join must be implemented.  If you
+			// have not implemented join yet, then comment out the
+			// call to join and instead uncomment the loop with
+			// yields; the loop has the same effect, but is a kludgy
+			// way to do it.
+			// ping.join();
+			for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
+		}
 	}
 
-        private Lock conditionLock;
+	// Invoke Condition2.selfTest() from ThreadedKernel.selfTest()
+
+	public static void selfTest() {
+		new InterlockTest();
+		sleepForTest1();
+	}
+
+	
+    private static void sleepForTest1 () {
+		Lock lock = new Lock();
+		Condition2 cv = new Condition2(lock);
+	
+		lock.acquire();
+		long t0 = Machine.timer().getTime();
+		System.out.println (KThread.currentThread().getName() + " sleeping");
+		// no other thread will wake us up, so we should time out
+		cv.sleepFor(2000);
+		long t1 = Machine.timer().getTime();
+		System.out.println (KThread.currentThread().getName() +
+					" woke up, slept for " + (t1 - t0) + " ticks");
+		lock.release();
+		}
 }
